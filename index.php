5534bd577859d2cc8ceb735cf63252c2fd2f176b
Alexander Kashyap <alexander.kashyap@gmail.com>
2021-12-02 09:33:49 -0600

    fixing wrong commit mb

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     </head>
     <body>
     <?php
+
+        #ALEXANDER KASHYAP DEC 1 2021
+        
         include("creds.php");
         include("library.php");
         try {
@@ -12,108 +15,52 @@
             $dsn = "mysql:host=courses;dbname=z1926618";
             $pdo = new PDO($dsn, $username, $password);
 
-            # 1
-            $rs = $pdo->query("SELECT * FROM S;");
+            # Showing all of the products
+            echo "ATG, Product list...";
+            $rs = $pdo->query("SELECT * FROM PRODUCTS;");
             $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
             draw_table($rows);
 
-            #2
-            $rs = $pdo->query("SELECT * FROM P;");
-            $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
-            draw_table($rows);
-
-            ### the form for all other requirments ###
+            ### the form ###
             # choosing a part
             echo '<form method="post">';
-            echo '<label for="parts">Choose a part:</label>';
+            echo '<br><label for="parts">Select Part:</label>';
             echo '<select id="parts" name="parts">';
-            $rs = $pdo->query("SELECT DISTINCT PNAME FROM P;");
+            $rs = $pdo->query("SELECT DISTINCT PRODUCT_NAME FROM PRODUCTS;");
             $row = $rs->fetchAll(PDO::FETCH_ASSOC);
 
-            foreach ($row as $item) {
+            foreach ($row as $item) {   
                 foreach ($item as $it) {
                     echo "<option value=".$it.">".$it."</option>";
                 }
             }
+            echo '</select><br>';
 
-            #ch0osing a supplier
-            echo "</select>";
-            echo '<br><label for="parts">Choose a Supplier:</label>';
-            echo '<select id="supplier" name="supplier">';
-            $rs = $pdo->query("SELECT S FROM S;");
-            $row = $rs->fetchAll(PDO::FETCH_ASSOC);
-           
-            foreach ($row as $item) {
-                foreach ($item as $it) {
-                    echo "<option value=".$it.">".$it."</option>";
-                }
-            }
-            echo "</select>";
+            #select the quantity for that part
+            echo '<label for="quantity">Select Quantity:</label>';
+            echo '<input type="text" id="quantity" name="quantity"><br>';
 
-            # purchase form
-            echo '<br><label for="parts">PURCHASE:Choose Part, Supplier, AMT:</label>';
-            echo '<select id="p" name="p">';
-            $rs = $pdo->query("SELECT P FROM P;");
-            $row = $rs->fetchAll(PDO::FETCH_ASSOC);
-         
-            foreach ($row as $item) {
-                foreach ($item as $it) {
-                    echo "<option value=".$it.">".$it."</option>";
-                }
-            }
-            echo "</select>";
-            echo '<select id="s" name="s">';
-            $rs = $pdo->query("SELECT S FROM S;");
-            $row = $rs->fetchAll(PDO::FETCH_ASSOC);
-            
-            foreach ($row as $item) {
-                foreach ($item as $it) {
-                    echo "<option value=".$it.">".$it."</option>";
-                }
-            }
-            echo "</select>";
-            echo '<input type="text" name="q">';
-
-            #input a new part
-            echo '<br>Part to add info[P, PNAME, COLOR, WEIGHT]<input type="text" name="pp">';
-            echo '<input type="text" name="pname">';
-            echo '<input type="text" name="color">';
-            echo '<input type="text" name="weight">';
-
-            #input a new supplier
-            echo '<br>Supplier to add info[S, SNAME, STATUS, CITY]<input type="text" name="ss">';
-            echo '<input type="text" name="sname">';
-            echo '<input type="text" name="status">';
-            echo '<input type="text" name="city">';
-
-            #submits
-            echo '<br><input type="submit" value="Submit" />';
+            #add to cart
+            echo '<br><input type="submit" value="Add to Cart" />';
             echo "</form>";
 
-            #inserting the new part/supplier
-            $pdo->exec('INSERT INTO S (S, SNAME, CITY, STATUS) VALUES("'.$_POST["ss"].'", "'.$_POST['sname'].'", "'.$_POST['city'].'", '.$_POST["status"].");");
-            $pdo->exec('INSERT INTO P (P, PNAME, COLOR, WEIGHT) VALUES("'.$_POST["pp"].'", "'.$_POST['pname'].'", "'.$_POST['color'].'", '.$_POST["weight"].");");
 
-            #showing what the suppliers carry
-            echo "Suppliers who carry ".$_POST["parts"]."s:<br>";
-            $rs = $pdo->query('SELECT S, QTY, COLOR, WEIGHT FROM SP, P WHERE SP.P=P.P AND PNAME="'.$_POST["parts"].'";');
-            $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
-            foreach ($rows as $row) {
-                echo $row["S"]." has ".$row["QTY"]." ".$row["COLOR"]." ".$_POST["parts"]."s of weight ".$row["WEIGHT"]."<br>";
+            #go to shopping cart screen
+            echo '<br><form action="https://google.com">';
+            echo '<input type="submit" value="Checkout/Go to Cart" />';
+            echo '</form>';
+
+            #go to employee screen
+            echo '<br><br><br><br><form action="https://google.com">';
+            echo '<input type="submit" value="Edit/Employee View" />';
+            echo '</form>';
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+                #inserting the items chosen into shopping cart
+                $prepared = $pdo->prepare('INSERT INTO CART (PRODUCT_NAME, QUANTITY, COST) VALUES(?, ?, ?);');
+                $prepared->execute(array($_POST["parts"], $_POST["quantity"], 1.99));
             }
-
-            #showing info on the supplier chosen
-            echo "<br>Info on Supplier: ".$_POST["supplier"]."<br>";
-            $rs = $pdo->query('SELECT S.S, SNAME, STATUS, CITY, P, QTY FROM SP, S WHERE SP.S=S.S AND S.S="'.$_POST["supplier"].'";');
-            $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
-            draw_table($rows);
-
-            #updating the qty
-            $pdo->exec("UPDATE SP SET QTY = QTY - ".$_POST["q"].' WHERE P="'.$_POST["p"].'" AND S="'.$_POST["s"].'";');
-            echo "<br> Updated Quantities: <br>";
-            $rs = $pdo->query("SELECT * FROM SP;");
-            $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
-            draw_table($rows);
 
 
 
